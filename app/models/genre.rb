d7d8cc41cb4818f4025162f3f4992e85473c5e01
noriyoshi-82 <tomoyoshi.tomo1111@gmail.com>
2020-09-15 19:17:35 +0900

    修正

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,6 @@
 class Genre < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
+    { id: 1, name: 'ジャンルを選択' },
     { id: 2, name: ' 勉強 '},
     { id: 4, name: ' 健康 ' },
     { id: 5, name: ' お金 ' },
