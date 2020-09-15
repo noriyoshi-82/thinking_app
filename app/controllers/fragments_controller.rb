d6295904a2c53dcb7e772d39f3973c1fb4b637d2
noriyoshi-82 <tomoyoshi.tomo1111@gmail.com>
2020-09-15 14:41:30 +0900

    投稿機能実装

--- app/controllers/fragments_controller.rb
+++ app/controllers/fragments_controller.rb
@@ -2,4 +2,23 @@ class FragmentsController < ApplicationController
   def index
     @fragments = Fragment.all
   end
+
+  def new 
+    @fragment = Fragment.new
+  end
+  
+
+  def create
+    @fragment = Fragment.new(fragment_params)
+    if @fragment.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def fragment_params
+    params.require(:fragment).permit(:title, :genre, :heading_a, :heading_b, :heading_c, :heading_a_a, :heading_a_b, :heading_b_a, :heading_b_b, :heading_c_a, :heading_c_b, :free_memo).merge(user_id: current_user.id)
+  end
 end
--- app/models/fragment.rb
+++ app/models/fragment.rb
@@ -1,3 +1,9 @@
 class Fragment < ApplicationRecord
   belongs_to :user
+
+  with_options presence :true do
+    validates :title
+    validates :genre
+    validates :heading_a
+  end
 end
